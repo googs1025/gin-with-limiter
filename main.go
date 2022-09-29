232f7a66d518f67a4bc1e31a42e7dd880fb9a38c
googs1025 <googs1025@gmail.com>
2022-09-29 23:28:26 +0800

    feat: 新增定期加入token方法

--- main.go
+++ main.go
@@ -2,21 +2,20 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"golanglearning/new_project/gin-with-limiting/src"
 )
+
+func test(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "ok"})
+}
 
 func main() {
 
-	//ratelimter := src.NewBucket(10)
+
 
 	r := gin.New()
-	// 不可能每个请求都写判断，所以使用装饰器模式。
-	//r.GET("/", func(c *gin.Context) {
-	//	if ratelimter.IsAccept() {
-	//		c.JSON(200, gin.H{"message": "ok"})
-	//	} else {
-	//		c.AbortWithStatusJSON(400, gin.H{"message":"fail by rate limit"})
-	//	}
-	//})
+	// 使用装饰器模式。
+	r.GET("/", src.Limiter(10)(test))
 
 	defer func() {
 		r.Run(":8081")
--- src/Bucket.go
+++ src/Bucket.go
@@ -3,28 +3,59 @@ package src
 import (
 	"log"
 	"sync"
+	"time"
 )
 
 type Bucket struct {
 	cap int
 	tokens int
 	lock sync.Mutex
+	rate int // 每秒加入的令牌数
 }
 
-func NewBucket(cap int) *Bucket {
-	if cap <= 0 {
-		log.Fatal("cap can't == 0!")
+func NewBucket(cap int, rate int) *Bucket {
+	if cap <= 0 || rate <= 0 {
+		log.Fatal("config wrong!")
 	}
-	return &Bucket{
+	b := &Bucket{
 		cap: cap,
 		tokens: cap,
+		rate: rate,
+	}
+	b.start()
+
+	return b
+}
+
+// 使用goroutine的方式定时加入tokens
+// start 加入tokens
+func (b *Bucket) start() {
+	for {
+		time.Sleep(time.Second)
+		go b.addToken()
 	}
 }
 
+// addToken 加入token的方法
+func (b *Bucket) addToken() {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+	// 如果 tokens:4 + rate:3 > cap:5 ，那就无法加入
+	if b.tokens + b.rate <= b.cap {
+		b.tokens += b.rate
+	} else {
+		b.tokens = b.cap
+	}
+
+
+}
+
+// IsAccept 是否接受请求
 func (b *Bucket) IsAccept() bool {
 	b.lock.Lock()
 	defer b.lock.Unlock()
 	if b.tokens > 0 {
+		b.tokens--
 		return true
 	}
 
