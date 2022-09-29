6af51b75fb9698c0d49581ed19b56c29819db69d
googs1025 <googs1025@gmail.com>
2022-09-29 23:14:02 +0800

    最简易版

--- main.go
+++ main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"golanglearning/new_project/gin-with-limiting/src"
 )
 
 func main() {
 
-	//ratelimter := src.NewBucket(10)
+	// 最简易的限流，没有引入中间件模式
+	ratelimter := src.NewBucket(10)
 
 	r := gin.New()
-	// 不可能每个请求都写判断，所以使用装饰器模式。
-	//r.GET("/", func(c *gin.Context) {
-	//	if ratelimter.IsAccept() {
-	//		c.JSON(200, gin.H{"message": "ok"})
-	//	} else {
-	//		c.AbortWithStatusJSON(400, gin.H{"message":"fail by rate limit"})
-	//	}
-	//})
+	//不可能每个请求都写判断，所以使用装饰器模式。
+	r.GET("/", func(c *gin.Context) {
+		if ratelimter.IsAccept() {
+			c.JSON(200, gin.H{"message": "ok"})
+		} else {
+			c.AbortWithStatusJSON(400, gin.H{"message":"fail by rate limit"})
+		}
+	})
 
 	defer func() {
 		r.Run(":8081")
